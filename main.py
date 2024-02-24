d64328fc13648589c380e416a4d7737339faf257
Игорь <dgtggdhhf@gmail.com>
2024-02-24 17:32:56 +0200

    2.0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 url = "https://kups.club/"
 
-header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
 session = requests.session()
 
 for j in range(1, 10):
